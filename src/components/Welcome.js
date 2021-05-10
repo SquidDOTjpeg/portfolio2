1aafa12bdabb7f66931eeffa3b3b145a0c811e6e
Anthony Pillow <anthonypillow18@gmail.com>
2021-05-10 00:46:40 -0700

    Started working on some css animations for the site and tried getting the welcome page flex box to be more organized

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,15 @@
 import "./Welcome.css"
+import arrow from "../assets/Arrow.png"
 
 function Welcome() {
     return (
         <div className="welcome-container">
             <div className="flex-item">
-                <p><span className="beeg">Hello!</span><span className="">Thanks for visiting.</span></p>
+                <p><span className="beeg">Hello!</span></p>
+                <p><span className="smol">Thanks for visiting.</span></p>
             </div>
             <div className="flex-item">
-                <img src="dont have one yet" alt="arrow"/>
+                <img src={arrow} alt="arrow" className="arrow"/>
             </div>
         </div>
     )
